206db6986b443ed7f3a1b37f1137033e453dd107
jpujol <jpujol@sovinty.com>
2016-06-03 11:34:00 +0200

    Add unit testing on oauth

--- test/withings-oauth2-spec.js
+++ test/withings-oauth2-spec.js
@@ -10,5 +10,76 @@ var error = new Error('ERROR');
 
 describe('Withings API Client:', function () {
 
+    describe('OAuth functionality:', function () {
+
+        beforeEach(function (done) {
+            options = {
+                consumerKey: 'consumerKey',
+                consumerSecret: 'consumerSecret',
+                callbackUrl: 'http://localhost:3000/oauth_callback'
+            };
+            client = new Withings(options);
+            done();
+        });
+
+        it('get an OAuth request token', function (done) {
+            var callback = sinon.spy();
+            sinon.stub(client.oauth, 'getOAuthRequestToken', function (cb) {
+                cb.call(void 0, null, 'token', 'tokenSecret');
+            });
+            client.getRequestToken(callback);
+
+            expect(callback.calledWith(null, 'token', 'tokenSecret')).to.be.true;
+
+            client.oauth.getOAuthRequestToken.restore();
+            done();
+        });
+
+        it('generate authorization URL', function (done) {
+            var url = client.authorizeUrl('token', 'tokenSecret');
+            expect(url).to.exist;
+            done();
+        });
+
+        it('generate an access token', function (done) {
+            var callback = sinon.spy();
+            sinon.stub(client.oauth, 'getOAuthAccessToken', function (r, rs, v, cb) {
+                expect(r).to.eq('requestToken');
+                expect(rs).to.eq('requestTokenSecret');
+                expect(v).to.eq('verifier');
+                cb.call(void 0, null, 'token', 'tokenSecret');
+            });
+            client.getAccessToken('requestToken', 'requestTokenSecret', 'verifier', callback);
+
+            expect(callback.calledWith(null, 'token', 'tokenSecret')).to.be.true;
+
+            client.oauth.getOAuthAccessToken.restore();
+            done();
+        });
+
+        it('error when making an unauthorized API call', function (done) {
+            try {
+                client.apiCall('https://test.api.endpoint', function () {
+                });
+            } catch (ex) {
+                expect(ex.message).to.eq('Authenticate before making API calls');
+                done();
+            }
+        });
+
+        it('error when making an API call with no user ID', function (done) {
+            client.accessToken = 'accessToken';
+            client.accessTokenSecret = 'accessTokenSecret';
+            try {
+                client.apiCall('https://test.api.endpoint', function () {
+                });
+            } catch (ex) {
+                expect(ex.message).to.eq('API calls require a user ID');
+                done();
+            }
+        });
+
+    });
+
 
 });
